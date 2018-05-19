ba8f73467b5a021c7829d0151d47da25b9c6c011
Wesley Harvey <pinecone062@gmail.com>
2018-05-19 10:05:30 -0700

    perl check.pl: beginning implementation

---
+++ perl/check.pl
@@ -0,0 +1,21 @@
+#! /usr/bin/env perl
+
+# perl/check.pl
+# ====================================================
+# CREATED: 2018-05-19
+# VERSION 0.0.1
+# AUTHOR: wlharvey4
+# ABOUT: Test runner for perl implementations
+# NOTES:
+# ----------------------------------------------------
+
+use strict;
+use warnings;
+
+sub check {
+    my $cc = $ARGV[0];
+}
+
+
+my $result = check();
+report($result);
