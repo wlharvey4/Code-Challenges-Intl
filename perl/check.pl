c07b6350a388419da4f8248858f674069a129a54
Wesley Harvey <pinecone062@gmail.com>
2018-05-21 10:52:24 -0700

    Added isUnique.pm; updated check.pl to use Perl boolean values

--- perl/check.pl
+++ perl/check.pl
@@ -10,29 +10,34 @@
 # NOTES:
 # ----------------------------------------------------
 
+# pragmas
 use strict;
 no  strict "refs"; # allows aliasing using variables
 use warnings;
 use v5.16;
 use lib '..';	# gives access to code challenges as modules
+use boolean;	# gives access to Perl boolean values
 
-use File::Spec::Functions; # provides &catfile()
-use Data::Printer output => 'stdout'; # provides &p() (pretty-printer)
+# Utility libraries
+use File::Spec::Functions; # provides &catfile() for portable path creation
+use Data::Printer output => 'stdout'; # provides &p() (pretty-printing of data structures)
 use JSON;		# provides &decode_json()
 use Test::Deep::NoTest; # provides &eq_deeply()
 
+# Variables
 our $fn;	# the aliased code challenge subroutine
 my $cc;		# the name of the code challenge from the CL invocation
 my $ccPM;	# the code challenge module path
 my $ccJSON;	# the code challenge data path
 my $json;	# the code challenge data storage
 
-$cc = $ARGV[0]; # the code challenge needs to be give on command-line as argument
+# get the Code Challenge name from the command-line argument
+$cc = $ARGV[0];
 unless ($cc) {
     die qq{You need to include the name of a code challenge: i.e., './check <code-challenge>'}
 }
 
-# construct the paths portably
+# construct paths to resources
 $ccPM = catfile(${cc}, 'perl', ${cc}.'.pm');
 $ccJSON = catfile('..', ${cc}, ${cc}.'.json');
 
@@ -44,11 +49,13 @@ $json = eval { # load and decode the test data into a Perl hash reference
     decode_json(<JSON>);
 } || die $@;
 
+# deeply test the Code Challenge results against the expected values
 our ($i, $w) = (0, 0); # $i :=  test number; $w := number of tests found wrong
 for my $test (@$json) {
     $i++;
     my $p = $test->{params};
     my $e = $test->{expected};
+    if ($e->isa("JSON::PP::Boolean")) { $e = boolean($e) } # need to convert JSON boolean to Perl boolean
     my $r = fn($p);
     reportError($p, $r, $e)  unless eq_deeply($r, $e);
 }
