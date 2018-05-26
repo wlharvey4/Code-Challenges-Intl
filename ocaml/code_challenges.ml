611978d0579993f988265ad092dc89726880b353
Wesley Harvey <pinecone062@gmail.com>
2018-05-25 20:02:48 -0700

    OCaml code challenges
    Implemented two code challenges and functor for checking them;
    fizzbuzz.ml
    isUnique.ml
    sample checks

--- ocaml/code_challenges.ml
+++ ocaml/code_challenges.ml
@@ -14,7 +14,7 @@ sig
   type in_t
   type out_t
   val fn : in_t -> out_t
-  val print_result : out_t -> unit
+  val print_fn : in_t -> unit
 end
 
 module type FIZZBUZZ =
@@ -23,7 +23,7 @@ sig
   type out_t
   val fizzbuzz : in_t -> out_t
   val fn : in_t -> out_t
-  val print_result : out_t -> unit
+  val print_fn : in_t -> unit
 end
 
 module Fizzbuzz : FIZZBUZZ =
@@ -43,8 +43,10 @@ struct
     | (true, false) -> Fizz
     | (false, true) -> Buzz
     | _ -> Num(n)
-  let print_result r = print_endline(
-    match r with
+  let print_fn n =
+    let result = fizzbuzz n in
+    print_endline((string_of_int n) ^ " -> " ^
+    match result with
       | Num n -> string_of_int n
       | Fizz -> "Fizz"
       | Buzz -> "Buzz"
@@ -58,7 +60,7 @@ sig
   type out_t
   val isUnique : in_t -> out_t
   val fn : in_t -> out_t
-  val print_result : out_t -> unit
+  val print_fn : in_t -> unit
 end
 
 module IsUnique : ISUNIQUE =
@@ -66,22 +68,37 @@ struct
   type in_t = string
   type out_t = bool
   let isUnique str =
-    match str with
-    | "a" -> true
-    | "b" -> false
-    | _ -> true
-  let print_result r = print_endline (string_of_bool r)
+    let rec eval i =
+      try
+        let c = String.get str i in
+        let f = String.contains_from str (i+1) c in
+        match f with
+        | true -> false
+        | false -> eval (i + 1)
+      with 
+      | Invalid_argument _ -> true
+      | e -> raise e
+    in eval 0
+  let print_fn s =
+    let result = isUnique s in
+    print_endline (s ^ " -> " ^ string_of_bool result)
   let fn = isUnique
 end
 
 module CodeChallenge = functor (CodeChall : CODECHALL) ->
   struct
     let fn input = CodeChall.fn input
-    let print_result result = CodeChall.print_result result
+    let print_fn input = CodeChall.print_fn input
   end
 
 module Fn1 = CodeChallenge(Fizzbuzz)
 module Fn2 = CodeChallenge(IsUnique)
 
-let () = Fn1.print_result (Fn1.fn 3)
-let () = Fn2.print_result (Fn2.fn "a")
+let () = Fn1.print_fn 3
+let () = Fn1.print_fn 5
+let () = Fn1.print_fn 15
+let () = Fn1.print_fn 1
+let () = Fn2.print_fn "a"
+let () = Fn2.print_fn "abc"
+let () = Fn2.print_fn "abca"
+let () = Fn2.print_fn "aa"
