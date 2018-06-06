00f818e7ccf2405f879a3814b00b0941daf91e7f
Wesley Harvey <pinecone062@gmail.com>
2018-06-05 20:46:58 -0700

    python/check.py ver 1.0.0

--- python/check.py
+++ python/check.py
@@ -4,13 +4,13 @@
 # ====================================================
 # CREATED: 2018-05-29
 # UPDATED: 2018-06-05
-# VERSION: 0.2.0
+# VERSION: 1.0.0
 # AUTHOR: wlharvey4
-# ABOUT: Python code challenge runner
+# ABOUT: Python code challenge test runner
 # NOTES:
 # ----------------------------------------------------
 
-import sys
+import sys, json
 from importlib import import_module
 from pathlib import PurePath, Path
 
@@ -29,10 +29,30 @@ root = PurePath(Path.cwd()).parent
 ccDir = PurePath.joinpath(root, cc)
 ccJson = PurePath.joinpath(ccDir, cc).with_suffix(JSON)
 
+# import json
+with open(ccJson, "r") as ccJsonFile:
+    jsonData = json.load(ccJsonFile)
+    
 # import code challenge as function 'fn'
 sys.path.append(str(root))
 ccPackage = cc + '.' + PYTHON + '.' + cc
-fnMod = import_module(ccPackage)
-fn = getattr(fnMod, cc)
+ccMod = import_module(ccPackage)
+fn = getattr(ccMod, cc)
 
-print(fn({'n': 15}))
+# test iterator
+results = {'passed': 0, 'failed': 0}
+for item in jsonData:
+    p = item['params']
+    e = item['expected']
+    r = fn(p)
+    if e != r:
+        print("PARAM:\t\t{[params]}".format(item))
+        print("EXPECTED:\t{[expected]}".format(item))
+        print("RESULT:\t\t{}\n".format(r))
+        results['failed'] += 1
+    else:
+        results['passed'] += 1
+
+print("\nRESULTS:\t{} {}".format(PYTHON, cc))
+print("\tPASSED:\t{[passed]:2d}".format(results))
+print("\tFAILED:\t{[failed]:2d}".format(results))
