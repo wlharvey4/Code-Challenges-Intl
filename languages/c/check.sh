10bdbfb67fc8753ef2bf4e98f763f16ad195e49a
Wesley Harvey <pinecone062@gmail.com>
2018-06-20 09:31:23 -0700

    Added init rule to Makefile;
    init creates a new code challenge and installs a template util.h

--- languages/c/check.sh
+++ languages/c/check.sh
@@ -1,15 +1,37 @@
 # languages/c/check.sh
 # ====================================================
 # CREATED: 2018-06-19
-# VERSION: 1.0.0
+# UPDATED: 2018-06-20
+# VERSION: 1.1.0
 # AUTHOR: wlharvey4
 # ABOUT: shell script to get code challenge from the
 #        command line, update the Makefile with the
 #        requested code challenge, compile the code
-#        challenge dynamically, and invoke check.c
+#        challenge dynamically, and invoke check.c;
+#	 can be invoked with `init' to start a new
+#	 code challenge in C with a template `util.h'
 # NOTES:
 # ----------------------------------------------------
 
-[ -z $1 ] && { echo USAGE './check <code-challenge>'; exit; }
-[ -d ../../challenges/$1 ] || { echo Code Challenge \`$1\' does not exist; exit; }
-make CC=$1 --silent
+USAGE='USAGE: ./check <code-challenge> [init]'
+
+[ -z $1 ] && { echo ${USAGE}; exit; }
+
+[ $2 ] && {
+    [ $2 == 'init' ] && {
+	make CC=$1 init
+	exit
+    } || {
+	echo ${USAGE}
+	exit
+    }
+}
+
+[ -d ../../challenges/$1 ] && {
+    make CC=$1 --silent
+    exit;
+} || {
+    echo $1 does not exist
+    echo ${USAGE}
+    exit
+}
