f2e4b40278069024bf1cdf444161cf11c8cc27b9
wlharvey <pinecone062@gmail.com>
2018-09-10 22:10:50 -0700

    check.h Updated comments about being dependent upon Jansson

--- languages/c/check.h
+++ languages/c/check.h
@@ -6,8 +6,12 @@
    VERSION: v1.1.0
    AUTHOR: wlharvey4
    ABOUT: Header for check.c, C test runner
-   NOTES: This implementation is dependent upon Jansson (www.digip.org/jansson),
-          currently at ver 2.11 for JSON parsing; it installs by default into
+   NOTES: This implementation is dependent upon Jansson: see
+          o www.digip.org/jansson, 
+	  o https://github.com/akheron/jansson, and
+	  o https://jansson.readthedocs.io/en/latest/
+          currently at ver 2.11 for JSON parsing;
+	  it installs by default into
 	  /usr/local/lib and /usr/local/include.
    CHANGE-LOG:
    ....................................................
