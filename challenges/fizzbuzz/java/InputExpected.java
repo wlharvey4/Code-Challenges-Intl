3c4e8dacddfed5eb77da9f199240e6976d7729f3
Wesley Harvey <pinecone062@gmail.com>
2018-09-23 23:15:06 -0700

    InputExpected.java ver. 0.0.2 Incorporated Gson

--- challenges/fizzbuzz/java/InputExpected.java
+++ challenges/fizzbuzz/java/InputExpected.java
@@ -1,29 +1,39 @@
 /**
    challenges/fizzbuzz/java/InputExpected.java
-   ==================================================
+   =================================================
    CREATED: 2018-09-12
-   UPDATED:
-   VERSION: 0.0.1
+   UPDATED: 2018-09-23
+   VERSION: 0.0.2
    AUTHOR: wlharvey4
    ABOUT: Utility class for Fizzbuzz.java; holds the 
           input parameter and expected output from the
 	  conversion from the JSON structure.
    NOTES: 
    CHANGE-LOG:
+   ................................................
    --version 0.0.1 2018-09-12
      + Initial commit
-   --------------------------------------------------
+   .................................................
+   --version 0.0.2 2018-09-23
+     + Implemented CCI_InputExpected; incorporated
+       Gson
+   -------------------------------------------------
  */
 
 package challenges.fizzbuzz.java;
+import com.google.gson.*;
+import languages.java.*;
 
-public class InputExpected {
+public class InputExpected implements CCI_InputExpected {
+    private Gson ccJson;
+    private Gson gson_input;
+    private Gson gson_expected;
+
     private Input input;
     private Expected expected;
 
-    public InputExpected(Input input, Expected expected) {
-	this.input = input;
-	this.expected = expected;
+    public InputExpected(Gson ccJson) {
+	this.ccJson = ccJson;
     }
 
     public Input input() {
@@ -32,5 +42,11 @@ public class InputExpected {
 
     public Expected expected() {
 	return this.expected;
+    }
+
+    class Params {
+    }
+
+    class Expected {
     }
 }
