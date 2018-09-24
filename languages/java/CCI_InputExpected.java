49ceb047bf74c4f053e0145fffee5440a2200b69
Wesley Harvey <pinecone062@gmail.com>
2018-09-24 10:36:54 -0700

    CCI_InputExpected.java ver.0.0.3
    Experimenting with a data structure that will load all of the JSON data
    imported by Gson

--- languages/java/CCI_InputExpected.java
+++ languages/java/CCI_InputExpected.java
@@ -3,7 +3,7 @@
    =========================================================================
    CREATED: 2018-09-23
    UPDATED:
-   VERSION: 0.0.2
+   VERSION: 0.0.3
    AUTHOR:  wlharvey4
    ABOUT:   Interface for Input_Expected.java, which converts JSON into Java
    	    objects
@@ -16,6 +16,10 @@
    .........................................................................
    2018-09-23T23:42 Version 0.0.2
    - factored interface into class
+   .........................................................................
+   2018-09-24T10:30 Version 0.0.3
+   - experimenting with data structure that can load JSON from Gson; this code
+     compiles
    -------------------------------------------------------------------------
  */
 
@@ -23,10 +27,61 @@ package languages.java;
 
 public class CCI_InputExpected {
 
-    class Params {
+    Params params;
+    Expected expected;
 
+    CCI_InputExpected() {}
+
+    public String toString() {
+	return "CCI_InputExpected:\n\tParams: " + this.params + "\n\tExpected: " + this.expected;
     }
-    class Expected {
 
+    static class Params {
+	int n;
+
+	Params() {}
+	Params(int n) {
+	    this.n = n;
+	}
+	public String toString() {
+	    return "Params.n: " + this.n;
+	}
+    }
+
+    static class Expected {
+	Expected expected;
+	Expected() {}
+	Expected(FB fb) {
+	    expected = new ExpectedFB(fb);
+	}
+	Expected(int n) {
+	    expected = new ExpectedInt(n);
+	}
+    }
+
+    static class ExpectedInt extends Expected {
+	int expected;
+	ExpectedInt() {}
+	ExpectedInt(int e) {
+	    expected = e;
+	}
+	public String toString() {
+	    return "ExpectedInt.expected: " + this.expected;
+	}
+    }
+
+    static class ExpectedFB extends Expected {
+	FB expected;
+	ExpectedFB() {}
+	ExpectedFB(FB fb) {
+	    expected = fb;
+	}
+	public String toString() {
+	    return "ExpectedFB.expected: " + this.expected;
+	}
+    }
+
+    enum FB {
+	FIZZ, BUZZ, FIZZBUZZ
     }
 }
