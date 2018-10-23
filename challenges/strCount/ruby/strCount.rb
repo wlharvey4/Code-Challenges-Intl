41063e4a1ee529e22e1d60bff9f1180b49bbbc3b
Wesley Harvey <pinecone062@gmail.com>
2018-10-23 16:22:03 -0700

    strCount.rb ver.1.1.0 Added #main for command-line operation;
    -- refactored #inspect for better message

--- challenges/strCount/ruby/strCount.rb
+++ challenges/strCount/ruby/strCount.rb
@@ -3,7 +3,7 @@
 # =============================================================================
 # CREATED: 2018-10-23
 # UPDATED: 2018-10-23
-# VERSION: 0.0.1
+# VERSION: 1.1.0
 # AUTHOR : wlharvey4
 # ABOUT  : strCount challenge implemented in Ruby
 # NOTES  : String#scan iterates through the string matching the pattern, adding
@@ -12,6 +12,10 @@
 # .............................................................................
 # version 1.0.0 2018-10-23T15:50
 # -- initial implementation
+# .............................................................................
+# version 1.1.0 2018-10-23T16:20
+# -- added #main method for command-line operation;
+# -- refactored #inspect for better description
 # -----------------------------------------------------------------------------
 
 class StrCount
@@ -28,9 +32,18 @@ class StrCount
     @count.to_s
   end
   def inspect
-    "StrCount of #{str} and #{char} is #{count}"
+    "StrCount of '#{char}' in '#{str}' is #{count}"
   end
   def == c
     self.count == c
   end
+end
+
+def main str1, str2
+  strCount = StrCount.new({"str1"=>str1,"str2"=>str2})
+  p strCount
+end
+
+if $0 == __FILE__
+  main $*[0], $*[1]
 end
