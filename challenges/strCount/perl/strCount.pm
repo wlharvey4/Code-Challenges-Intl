cd8e92850d9270f0727247f8f4aa8101bd1eff0a
wlharvey <pinecone062@gmail.com>
2018-10-23 19:06:46 -0700

    CCI strCount.pm ver.2.0.0 Refactored Perl code to use OO paradyme

--- challenges/strCount/perl/strCount.pm
+++ challenges/strCount/perl/strCount.pm
@@ -1,26 +1,26 @@
 # challenges/strCount/perl/strCount.pm
-# ====================================================
+# =============================================================================
 # CREATED: 2018-06-13
-# UPDATED: 2018-09-07
-# VERSION: 1.1.0
-# AUTHOR: wlharvey4
-# USAGE: perl5 strCount.pm <str1> <str2>
-# ABOUT:  Create a  function  called `strCount()'  that
-# accepts 2 string arguments  and returns an integer of
-# the count of occurrences the 2nd argument is found in
-# the first  one.  If  no occurrences  can be  found, a
-# count of  0 should  be returned.  The  first argument
-# can be  an empty  string; The second  string argument
-# will always be of length 1.
-# NOTES: The  RE expresion  /$re/g returns an  array of
-# matches;  returning  the  scalar value  of  an  array
-# returns the array's size.
+# UPDATED: 2018-10-23
+# VERSION: 1.1.1
+# AUTHOR : wlharvey4
+# USAGE  : perl5 strCount.pm <str1> <str2>
+# ABOUT  : strCount challenge implemented in Perl
+# NOTES  : The  RE expresion  /$re/g returns an  array of matches;  returning
+#        : the  scalar value  of  an  array returns the array's size.
 # CHANGE_LOG:
-#	2018-09-07 ver 1.1.0:
-#	--Added package StrCount
-#	--converted into modulino, callable as either a
-#	  module or script
-# ----------------------------------------------------
+# .............................................................................
+# version 1.1.0 2018-09-07
+# -- A dded package StrCount
+# -- converted into modulino, callable as either a module or script
+# .............................................................................
+# version 2.0.0 2018-10-23T19:00
+# -- Added README.md; removed description of challenge from About
+# -- Completely refactored module using object-oriented paradyme in order to
+#    to compare with Ruby; much, much harder to do, with no benefit to
+#    readability and probably not worth the effort for this little code; but,
+#    nevertheless it was a good exercise in coding Perl using Objects.
+# -----------------------------------------------------------------------------
 
 package StrCount;
 
@@ -28,28 +28,63 @@ use strict;
 use warnings;
 use v5.16;
 
-# params => {str1 => <str1>, str2 => <str2>}, where <str1> is a
-#	    random length String value (may be the empty String)
-#           and <str2> is a single character String (may not be
-#	    empty String)
-# ==> <count> where <count> is an integer
+my $USAGE = qq{USAGE: strCount str1 str2
+where str1 is random length string (may be empty) and str2 is single character.\n};
+
+# @_ := input := {'str1' => string, 'str2' => string}
+# 'str1' is random length string (including empty string)
+# 'str2' is single character (may not be empty)
+# integer represents count of 'str2' in 'str1'
 sub strCount {
-    # @_ := {'str1' => string1, 'str2' => string2}
-    my $str1 = $_[0]->{'str1'};
-    my $str2 = $_[0]->{'str2'};
-    my @c = $str1 =~ /$str2/g;
-    return scalar @c;
+    my $class = shift;
+    my ($input) = @_;
+    my ($str1, $str2) = ($input->{str1}, $input->{str2});
+
+    my $self = bless {str => $str1, char => $str2}, $class;
+    $self->calculate();
 }
 
-# @_ := @ARGV (as below)
+sub calculate {
+    my $self = shift;
+    my $char = $self->char();
+    my @c = $self->str() =~ /$char/g;
+    $self->count(scalar @c);
+    $self;
+}
+
+sub str {
+    my $self = shift;
+    if (@_) { $self->{str} = shift }
+    $self->{str};
+}
+
+sub char {
+    my $self = shift;
+    if (@_) { $self->{char} = shift }
+    $self->{char};
+}
+
+sub count {
+    my $self = shift;
+    if (@_) { $self->{count} = shift }
+    $self->{count};
+}
+
+sub inspect {
+    my $self = shift;
+    my ($str, $char, $count) = ($self->str(), $self->char(), $self->count());
+    print "strCount of '$char' in '$str' is $count.\n";
+}
+
+# @ARGV := (string, string)
 sub main {
-    my ($str1, $str2) = @ARGV;
-    my $c = strCount({ str1 => $str1, str2 => $str2 });
-    say "There ", $c == 1 ? "is" : "are", " $c occurrence", $c == 1 ? '' : 's', " of $str2 in $str1";
+    my ($str1, $str2) = eval { @ARGV if @ARGV == 2 };
+    unless ($str1 && $str2) { print $USAGE; exit 1; }
+
+    my $sc = StrCount->strCount({ str1 => $str1, str2 => $str2 });
+    $sc->inspect();
 }
 
-# @ARGV := (<str1>, <str2>)  where each value is a random String;
-#          <str1> can be empty String; <str2> must be single character
 main(@ARGV) unless caller();
 
 1;
